de69423ed7b18e490be1abe9d5c7e0c783e4ef2a
Christopher Baker <cgbaker@gmail.com>
2015-05-28 12:17:25 -0400

    fixed type: yml to yaml

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -4,7 +4,7 @@ PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
 for f in "compose-stage1" "compose-stage2" "compose-stage3" "meta"; do 
   echo "> Updating PUBLIC_HOSTNAME in $f"
-  sed "s/PUBLIC_HOSTNAME/$PUBLIC_HOSTNAME/g" $f.template.yml > $f.yml
+  sed "s/PUBLIC_HOSTNAME/$PUBLIC_HOSTNAME/g" $f.template.yaml > $f.yaml
 done
 
 # deploy appliance blueprint
