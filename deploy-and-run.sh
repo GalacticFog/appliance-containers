dc9eb4e045f367a5d198d0cd670e564996a07bb6
Christopher Baker <cgbaker@gmail.com>
2015-05-28 16:41:36 -0400

    put all stages back into one file; multiple files necessitated external_links, which brought fragility in naming modified deploy-and-run to handle stages by starting services by name

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -3,16 +3,16 @@
 PUBLIC_HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
 echo "> PUBLIC_HOSTNAME=$PUBLIC_HOSTNAME"
 
-for f in "compose-stage1" "compose-stage2" "compose-stage3" "meta"; do 
+for f in "docker-compose" "meta"; do 
   echo "> Updating PUBLIC_HOSTNAME in $f"
   sed "s/PUBLIC_HOSTNAME/$PUBLIC_HOSTNAME/g" $f.template.yaml > $f.yaml
 done
 
 # deploy appliance blueprint
 
-sudo docker-compose -f compose-stage1.yaml up -d
+sudo docker-compose up -d db zookeeper kafka
 # restore DB
-sudo docker-compose -f compose-stage2.yaml up -d
+sudo docker-compose up -d meta
 # deploy blueprint in meta, parse topology, set config
 #sudo docker-compose -f compose-stage3.yaml up -d
 
