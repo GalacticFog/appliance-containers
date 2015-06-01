1bd2668e2ceeab5803befa84e149664f6c4ee202
Christopher Baker <cgbaker@gmail.com>
2015-06-01 08:46:39 -0400

    some fakery around loc and env

--- if-config-task/deploy-blueprint-set-config.sh
+++ if-config-task/deploy-blueprint-set-config.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+ENV_ID=1
+LOC_ID=1
+
 BLUEPRINTS_RESPONSE=$(cat /tmp/example_blueprints.json)
 #BLUEPRINTS_RESPONSE=$(curl -s meta:9000/blueprints)
 BLUEPRINT_ID=$(echo $BLUEPRINTS_RESPONSE | jq -r '.[] | select(.name == "infrastructure") | .id')
 echo "Infrastructure blueprint ID: $BLUEPRINT_ID"
 
-PAYLOAD='{"location_id": 1, "environment_id": 1}'
+PAYLOAD="{\"location_id\": $ENV_ID, \"environment_id\": $LOC_ID}"
 #DEPLOY_RESPONSE=$(echo $PAYLOAD | curl -s -d@- -X POST http://meta:9000/blueprints/$BLUEPRINT_ID/deploy)
 DEPLOY_RESPONSE=$(cat /tmp/example_deployment.json)
 
-SECURITY_ID=$(echo $DEPLOY_RESPONSE | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-security") | .instances[0].id')
-SECURITY_SERVICE_ID=$(echo $DEPLOY_RESPONSE | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-security") | .instances[0].service_id')
-BILLING_ID=$(echo $DEPLOY_RESPONSE  | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-billing")  | .instances[0].id')
-BILLING_SERVICE_ID=$(echo $DEPLOY_RESPONSE  | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-billing")  | .instances[0].service_id')
-DNS_ID=$(echo $DEPLOY_RESPONSE      | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-dns")      | .instances[0].id')
-DNS_SERVICE_ID=$(echo $DEPLOY_RESPONSE      | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-dns")      | .instances[0].service_id')
-LAUNCHER_ID=$(echo $DEPLOY_RESPONSE | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-launcher") | .instances[0].id')
-LAUNCHER_SERVICE_ID=$(echo $DEPLOY_RESPONSE | jq '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-launcher") | .instances[0].service_id')
+SECURITY_ID=$(echo $DEPLOY_RESPONSE | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-security") | .instances[0].id')
+SECURITY_SERVICE_ID=$(echo $DEPLOY_RESPONSE | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-security") | .instances[0].service_id')
+BILLING_ID=$(echo $DEPLOY_RESPONSE  | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-billing")  | .instances[0].id')
+BILLING_SERVICE_ID=$(echo $DEPLOY_RESPONSE  | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-billing")  | .instances[0].service_id')
+DNS_ID=$(echo $DEPLOY_RESPONSE      | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-dns")      | .instances[0].id')
+DNS_SERVICE_ID=$(echo $DEPLOY_RESPONSE      | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-dns")      | .instances[0].service_id')
+LAUNCHER_ID=$(echo $DEPLOY_RESPONSE | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-launcher") | .instances[0].id')
+LAUNCHER_SERVICE_ID=$(echo $DEPLOY_RESPONSE | jq -r '.root_cluster[] | select(.name == "infrastructure") | .nodes[] | select(.node_template_name == "gestalt-launcher") | .instances[0].service_id')
 
 echo "Security: $SECURITY_ID (service $SECURITY_SERVICE_ID)"
 echo "Billing: $BILLING_ID (service $BILLING_SERVICE_ID)"
@@ -31,3 +34,17 @@ echo "GESTALT_NODE=$DNS_ID"                 >> /tmp/dns.env
 echo "GESTALT_SERVICE=$DNS_SERVICE_ID"      >> /tmp/dns.env
 echo "GESTALT_NODE=$LAUNCHER_ID"            >> /tmp/launcher.env
 echo "GESTALT_SERVICE=$LAUNCHER_SERVICE_ID" >> /tmp/launcher.env
+
+
+read -r -d '' DB_CONFIG <<- EOM
+{
+[
+  {"op": "add", "path": "/host", "value": "db"},
+  {"op": "add", "path": "/port", "value": "5432"},
+  {"op": "add", "path": "/db_name", "value": "???"},
+  {"op": "add", "path": "/username", "value": "gestsaltdev"},
+  {"op": "add", "path": "/password", "value": "M8keitw0rk"}
+]
+}
+EOM
+#echo $DB_CONFIG | curl -X PATCH http://meta:9000/environments/$ENV_ID/config
