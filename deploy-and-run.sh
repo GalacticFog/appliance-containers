052447f1ce1a8256edb98c7be9d7120439221dd1
Christopher Baker <cgbaker@gmail.com>
2015-06-06 12:02:20 -0400

    added --no-recreate to ifDataLoad task

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -12,7 +12,7 @@ done
 
 docker-compose up -d --no-recreate db zookeeper kafka
 # restore DB
-sudo docker-compose up ifDataLoad
+sudo docker-compose up --no-recreate ifDataLoad
 docker-compose up -d --no-recreate meta
 # deploy blueprint in meta, parse topology, set config
 #docker-compose up --no-recreate ifConfigTask
