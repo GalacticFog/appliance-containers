0f5d62b6609fa07f13764a416bd67b07ced21fb3
Christopher Baker <cgbaker@gmail.com>
2016-07-31 10:31:25 -0400

    added node+docker build image

--- gitlab-build-container/build.sh
+++ gitlab-build-container/build.sh
@@ -1,2 +1,5 @@
 docker build -t galacticfog/docker-sbt:1.7 .
 docker push galacticfog/docker-sbt:1.7
+
+docker build -t galacticfog/docker-node:1.7 -f Dockerfile.node .
+docker push galacticfog/docker-node:1.7
