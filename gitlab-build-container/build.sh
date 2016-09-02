7bcdd38be7374681a909fe166427ead9661915a9
Christopher Baker <cgbaker@gmail.com>
2016-09-02 10:34:03 -0400

    added docker image for deploying into test cluster

--- gitlab-build-container/build.sh
+++ gitlab-build-container/build.sh
@@ -3,7 +3,10 @@
 set -e 
 
 docker build -t galacticfog/docker-sbt:1.7 -f Dockerfile.sbt .
-docker push galacticfog/docker-sbt:1.7
+docker push     galacticfog/docker-sbt:1.7
 
 docker build -t galacticfog/docker-node:1.7 -f Dockerfile.node .
-docker push galacticfog/docker-node:1.7
+docker push     galacticfog/docker-node:1.7
+
+docker build -t galacticfog/gitlab-updater:latest -f Dockerfile.updater .
+docker push     galacticfog/gitlab-updater:latest
---
+++ gitlab-build-container/update-deployed-app.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+#set -x
+
+if [ $# -lt 3 ]; then 
+  echo "usage: [marathon-url] [app-id] [image]"
+  exit 1
+fi
+
+MARATHON=$1
+APPID=$2
+NEWIMG=$3
+
+echo querying $APPID at $MARATHON
+echo 
+
+http --check-status HEAD $MARATHON/v2/apps/$APPID
+current=$(http --check-status GET $MARATHON/v2/apps/$APPID)
+echo 
+echo Current image: $(echo $current | jq '.app.container.docker.image')
+update=$(echo $current | jq ".app.container | .docker.image = \"$NEWIMG\" | { \"container\": {\"docker\": .docker}, \"upgradeStrategy\": { \"minimumHealthCapacity\": 0.0, \"maximumOverCapacity\": 1.0 } }")
+echo "Update payload:"
+echo $update
+echo $update | http --check-status PUT $MARATHON/v2/apps/$APPID
